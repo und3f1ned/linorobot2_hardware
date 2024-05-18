9a0050a13ef8512f03a4d3ba71ce89aa00e3f439
Thomas Chou <thomas@wytron.com.tw>
2024-05-18 13:10:14 -0700

    action: Fibonacci action server demo
    Signed-off-by: Thomas Chou <thomas@wytron.com.tw>

--- config/custom/gendrv_wifi_config.h
+++ config/custom/gendrv_wifi_config.h
@@ -239,6 +239,7 @@ ROBOT ORIENTATION
 // #define BOARD_INIT_LATE {}
 // #define BOARD_LOOP {}
 // #define ADDTWO_SERVICE
+// #define FIBONACCI_SERVER
 
 #ifdef USE_SYSLOG
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){ \
--- config/custom/pico_config.h
+++ config/custom/pico_config.h
@@ -244,6 +244,7 @@ const int16_t ADC_LUT[4096] = { /* insert adc_calibrate data here */ };
 // #define BOARD_INIT_LATE {}
 // #define BOARD_LOOP {}
 // #define ADDTWO_SERVICE
+// #define FIBONACCI_SERVER
 
 #ifdef USE_SYSLOG
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){ \
