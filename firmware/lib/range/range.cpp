7acc3496758ee7992efc18197d9b807d05889961
und3f1ned <dev@xedin.ru>
2024-12-31 19:38:06 +0300

    range: replace sonar with infared tfmini-s

--- firmware/lib/range/range.cpp
+++ firmware/lib/range/range.cpp
@@ -1,40 +1,59 @@
+#include "config.h"
 #include <Arduino.h>
+#include <Wire.h>
 #include <micro_ros_utilities/string_utilities.h>
 #include <sensor_msgs/msg/range.h>
-#include "config.h"
 
-//define sound speed in m/uS
-#define SOUND_SPEED 0.00034
-#define TIMEOUT 5000 // uS
-#define FOV (15 * 0.0174533) // field of view rad
-#define MIN_RANGE 0.02 // 2cm
-#define MAX_RANGE (SOUND_SPEED * TIMEOUT / 2 * 0.95)
-// the 5000uS timeout limits range to roughly 0.8m
+#define TFMINI_I2C_ADDR 0x10
+#define FOV (2.0 * 0.0174533) // 2 degrees to radians
+#define MIN_RANGE 0.30        // 30cm
+#define MAX_RANGE 12.00       // 12m
 
 sensor_msgs__msg__Range range_msg_;
-sensor_msgs__msg__Range getRange()
-{
-#ifdef TRIG_PIN // ultrasonic sensor HC-SR04
-    unsigned long duration;
-    // ping
-    // Sets the trigPin on HIGH state for 10 micro seconds
-    digitalWrite(TRIG_PIN, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(TRIG_PIN, LOW);
-    // Reads the echoPin, returns the sound wave travel time in microseconds
-    duration = pulseIn(ECHO_PIN, HIGH, TIMEOUT);
-    range_msg_.range = duration ? (duration * SOUND_SPEED / 2) : +INFINITY;
-    range_msg_.field_of_view = FOV;
-    range_msg_.min_range = MIN_RANGE;
-    range_msg_.max_range = MAX_RANGE;
-#endif
-    return range_msg_;
+
+uint16_t readDistance() {
+  Wire.beginTransmission(TFMINI_I2C_ADDR);
+  Wire.write(0x01); // Distance data register
+  Wire.endTransmission();
+
+  if (Wire.requestFrom(TFMINI_I2C_ADDR, 2) == 2) {
+    uint16_t distance = Wire.read() | (Wire.read() << 8);
+    return distance;
+  }
+  return 0;
 }
 
-void initRange()
-{
-#ifdef TRIG_PIN // ultrasonic sensor HC-SR04
-    range_msg_.header.frame_id = micro_ros_string_utilities_set(range_msg_.header.frame_id, "sonar_link");
-    pinMode(TRIG_PIN, OUTPUT);
-#endif
+uint16_t readStrength() {
+  Wire.beginTransmission(TFMINI_I2C_ADDR);
+  Wire.write(0x03); // Signal strength register
+  Wire.endTransmission();
+
+  if (Wire.requestFrom(TFMINI_I2C_ADDR, 2) == 2) {
+    uint16_t strength = Wire.read() | (Wire.read() << 8);
+    return strength;
+  }
+  return 0;
+}
+
+sensor_msgs__msg__Range getRange() {
+  uint16_t distance = readDistance();
+  uint16_t strength = readStrength();
+
+  if (strength > 100 && distance < 1200) {
+    range_msg_.range = distance / 100.0; // Convert to meters
+  } else {
+    range_msg_.range = +INFINITY;
+  }
+
+  range_msg_.field_of_view = FOV;
+  range_msg_.min_range = MIN_RANGE;
+  range_msg_.max_range = MAX_RANGE;
+
+  return range_msg_;
+}
+
+void initRange() {
+  range_msg_.header.frame_id =
+      micro_ros_string_utilities_set(range_msg_.header.frame_id, "tfmini_link");
+  range_msg_.radiation_type = sensor_msgs__msg__Range__INFRARED;
 }
--- firmware/lib/range/range.h
+++ firmware/lib/range/range.h
@@ -1,8 +1,16 @@
 #ifndef RANGE_H
 #define RANGE_H
 
+#include <Wire.h>
 #include <sensor_msgs/msg/range.h>
+
+#define TFMINI_I2C_ADDR 0x10
+#define TFMINI_DISTANCE_REG 0x01
+#define TFMINI_STRENGTH_REG 0x03
+
 sensor_msgs__msg__Range getRange();
 void initRange();
+uint16_t readDistance();
+uint16_t readStrength();
 
 #endif
