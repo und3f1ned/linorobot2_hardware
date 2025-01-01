28282ac5a4b8e64bc8d3c4392b1f3cff303c3851
und3f1ned <dev@xedin.ru>
2025-01-01 11:57:48 +0300

    range: rewrite with I2Cdev

--- firmware/lib/range/range.cpp
+++ firmware/lib/range/range.cpp
@@ -1,43 +1,54 @@
-#include "config.h"
+// range.cpp
+#include "range.h"
 #include <Arduino.h>
-#include <Wire.h>
 #include <micro_ros_utilities/string_utilities.h>
-#include <sensor_msgs/msg/range.h>
 
-#define TFMINI_I2C_ADDR 0x10
-#define FOV (2.0 * 0.0174533) // 2 degrees to radians
-#define MIN_RANGE 0.30        // 30cm
-#define MAX_RANGE 12.00       // 12m
+TFMiniI2C tfmini;
 
-sensor_msgs__msg__Range range_msg_;
+TFMiniI2C::TFMiniI2C() {}
 
-uint16_t readDistance() {
-  Wire.beginTransmission(TFMINI_I2C_ADDR);
-  Wire.write(0x01); // Distance data register
-  Wire.endTransmission();
+bool TFMiniI2C::init() {
+  // Initialize I2Cdev
+  i2c_dev.initialize();
 
-  if (Wire.requestFrom(TFMINI_I2C_ADDR, 2) == 2) {
-    uint16_t distance = Wire.read() | (Wire.read() << 8);
+  // Configure range message
+  range_msg_.header.frame_id =
+      micro_ros_string_utilities_set(range_msg_.header.frame_id, "tfmini_link");
+  range_msg_.radiation_type = sensor_msgs__msg__Range__INFRARED;
+  range_msg_.field_of_view = FOV;
+  range_msg_.min_range = MIN_RANGE;
+  range_msg_.max_range = MAX_RANGE;
+
+  // Test connection
+  uint8_t test_data;
+  return i2c_dev.readByte(TFMINI_I2C_ADDR, 0x00, &test_data);
+}
+
+uint16_t TFMiniI2C::getDistance() {
+  uint16_t distance;
+  uint8_t buffer[2];
+
+  if (i2c_dev.readBytes(TFMINI_I2C_ADDR, TFMINI_DISTANCE_REG, 2, buffer)) {
+    distance = (buffer[1] << 8) | buffer[0];
     return distance;
   }
   return 0;
 }
 
-uint16_t readStrength() {
-  Wire.beginTransmission(TFMINI_I2C_ADDR);
-  Wire.write(0x03); // Signal strength register
-  Wire.endTransmission();
+uint16_t TFMiniI2C::getSignalStrength() {
+  uint16_t strength;
+  uint8_t buffer[2];
 
-  if (Wire.requestFrom(TFMINI_I2C_ADDR, 2) == 2) {
-    uint16_t strength = Wire.read() | (Wire.read() << 8);
+  if (i2c_dev.readBytes(TFMINI_I2C_ADDR, TFMINI_STRENGTH_REG, 2, buffer)) {
+    strength = (buffer[1] << 8) | buffer[0];
     return strength;
   }
   return 0;
 }
 
-sensor_msgs__msg__Range getRange() {
-  uint16_t distance = readDistance();
-  uint16_t strength = readStrength();
+sensor_msgs__msg__Range TFMiniI2C::getRangeMessage() {
+  uint16_t distance = getDistance();
+  uint16_t strength = getSignalStrength();
 
   if (strength > 100 && distance < 1200) {
     range_msg_.range = distance / 100.0; // Convert to meters
@@ -45,15 +56,10 @@ sensor_msgs__msg__Range getRange() {
     range_msg_.range = +INFINITY;
   }
 
-  range_msg_.field_of_view = FOV;
-  range_msg_.min_range = MIN_RANGE;
-  range_msg_.max_range = MAX_RANGE;
-
   return range_msg_;
 }
 
-void initRange() {
-  range_msg_.header.frame_id =
-      micro_ros_string_utilities_set(range_msg_.header.frame_id, "tfmini_link");
-  range_msg_.radiation_type = sensor_msgs__msg__Range__INFRARED;
-}
+// Global functions to maintain compatibility with existing code
+void initRange() { tfmini.init(); }
+
+sensor_msgs__msg__Range getRange() { return tfmini.getRangeMessage(); }
--- firmware/lib/range/range.h
+++ firmware/lib/range/range.h
@@ -1,16 +1,30 @@
+// range.h
 #ifndef RANGE_H
 #define RANGE_H
 
-#include <Wire.h>
+#include "I2Cdev.h"
 #include <sensor_msgs/msg/range.h>
 
 #define TFMINI_I2C_ADDR 0x10
 #define TFMINI_DISTANCE_REG 0x01
 #define TFMINI_STRENGTH_REG 0x03
 
-sensor_msgs__msg__Range getRange();
-void initRange();
-uint16_t readDistance();
-uint16_t readStrength();
+class TFMiniI2C {
+public:
+  TFMiniI2C();
+  bool init();
+  uint16_t getDistance();
+  uint16_t getSignalStrength();
+  sensor_msgs__msg__Range getRangeMessage();
+
+private:
+  I2Cdev i2c_dev;
+  sensor_msgs__msg__Range range_msg_;
+  const float FOV = 2.0 * 0.0174533; // 2 degrees to radians
+  const float MIN_RANGE = 0.10;      // 10cm
+  const float MAX_RANGE = 12.00;     // 12m
+};
+
+extern TFMiniI2C tfmini;
 
 #endif
